87125f615a3fa762ecdc1581109b30c665cb3372
gagandangwal17 <76488674+gagandangwal17@users.noreply.github.com>
2023-08-27 18:59:27 +0000

    media queries

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,42 @@
-import css from './src/test.module.scss'
+import css from "./home.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 
-export default function testpage(){
-    return(
-
+export default function Home() {
+    return (
         <div className={css.root}>
-            <div className={css.container}>
-                <div className="flex">
-                    <div className={css.left}>
-                        left
-                    </div>
-                    
-                    <div className={css.right}>
-                        left
-                    </div>
-                    
-                    <div className={css.doc}>
-                        doc
-                    </div>
+            <div className={css.left}>
+                <h2 className="text-4xl">Dr. Ted</h2>
+
+                <p>
+                    The paragraph here The paragraph here The paragraph here The
+                    paragraph here The paragraph hereThe paragraph here The
+                    paragraph here
+                </p>
+
+                <div className={css.field}>
+                    <span>Input</span>
+                    <textarea className={css.textarea} />
+                </div>
+
+                <button className={css.askbutton}>
+                    <span className={css.icon}>
+                        <FontAwesomeIcon icon={faMicrophone} />
+                    </span>
+                    <span className={css.text}>Ask my anything</span>
+                </button>
+            </div>
+
+            <div className={css.right}>
+                <div className={css.field}>
+                    <span>Response</span>
+                    <textarea className={css.textarea} />
                 </div>
             </div>
+
+            <div className={css.middle}>
+                <img src="/doc.png" alt="doc" />
+            </div>
         </div>
-        
-    )
+    );
 }
