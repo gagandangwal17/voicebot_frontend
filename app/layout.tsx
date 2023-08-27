d1e1adb66d6aae2a18c12d804a082756bad67386
Gagan Dangwal <gagandangwal175@gmail.com>
2023-08-27 13:27:34 +0530

    Changes added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,40 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import css from "./layout.module.scss";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Innov8 - Voice Assistant AI Chat bot',
-  description: 'Developed by Team Innov8',
-}
+    title: "Innov8 - Voice Assistant AI Chat bot",
+    description: "Developed by Team Innov8",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <div className={css.container}>
+                    <div className={css.header}>
+                        <div className={css.left}>
+                            <img src="/logo.png" alt="team innov8" />
+                        </div>
+                        <div className={css.right}>
+                            <h2>
+                                Designed & Developed <br /> Team Innov8
+                            </h2>
+                        </div>
+                    </div>
+                    {children}
+                </div>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,25 @@
-import Image from 'next/image'
-import css from "./home.module.scss"
-export default function Home() {
-  return (
-    <main className={css.root}>
-        <div className={css.rect1}>
+import css from './src/test.module.scss'
 
-          <div className={css.rect3}> 
+export default function testpage(){
+    return(
 
-          </div>
-
-          <div className={css.rect2}>
-
-            <div>
-              <img src="/logo.png" alt="Logo" className={css.logo} />
-            </div>
-
-            <div className={css.textContainer}>
-              <h1 className={css.name}>Dr. Ted</h1>
-
-              <p className=" mr-16 text-lg text-justify leading-6 text-gray-300">
-              Meet Dr. Ted, the epitome of an AI voice bot - always polite and incredibly helpful. 
-              With his advanced conversational skills, Dr. Ted is here to assist and provide seamless interactions.
-              </p>
-
-              <button className={css.button} type="button">Ask me anything!</button>
-
-              
+        <div className={css.root}>
+            <div className={css.container}>
+                <div className="flex">
+                    <div className={css.left}>
+                        left
+                    </div>
                     
+                    <div className={css.right}>
+                        left
+                    </div>
+                    
+                    <div className={css.doc}>
+                        doc
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-    </main>
-  )
+        
+    )
 }
